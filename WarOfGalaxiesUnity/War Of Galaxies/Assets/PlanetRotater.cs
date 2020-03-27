b5a9281655c786ca9dfd89e95fd8d7edf00b2085
akbalburak <akbalburak2@gmail.com>
2020-03-27 03:25:27 +0300

    Planets and their rotation included.

---
+++ WarOfGalaxiesUnity/War Of Galaxies/Assets/PlanetRotater.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlanetRotater : MonoBehaviour
+{
+    public float RotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, transform.up, RotateSpeed* Time.deltaTime);
+    }
+}
---
+++ WarOfGalaxiesUnity/War Of Galaxies/Assets/SolarSystemRotate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystemRotate : MonoBehaviour
+{
+    [Serializable]
+    public class SolarSystem
+    {
+        public List<SolarPlanet> Planets;
+        public SolarSystem()
+        {
+            Planets = new List<SolarPlanet>();
+        }
+    }
+    [Serializable]
+    public class SolarPlanet
+    {
+        public GameObject Planet;
+        public float PlanetRotationSpeed;
+        public float DistanceFromSun;
+    }
+
+    public SolarSystem System;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int ii = 0; ii < System.Planets.Count; ii++)
+        {
+            SolarPlanet planet = System.Planets[ii];
+            planet.Planet.transform.RotateAround(transform.position, transform.up,planet.PlanetRotationSpeed);
+        }
+    }
+}
